a088b1f068724349f192df5ca6e592c6e9b2b7fd
jvilla <josevillacinda96@gmail.com>
2021-03-25 22:44:13 -0700

    added develop folder

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const sequelize = require('./config/connection')
+// import sequelize connection
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(require('./routes'))
+
+// sync sequelize models to the database, then turn on the server
+sequelize.sync()
+.then(() => app.listen(process.env.PORT || 3000))
+.catch(err => console.log(err))
